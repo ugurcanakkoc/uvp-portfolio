ab398fca33d0f5186cc8accd1cefcbefeaafd556
Your Name <you@example.com>
2026-01-21 12:41:08 +0300

    style: standardize all image paths and files to lowercase for case-sensitivity

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,18 +7,18 @@ export const projects: Project[] = [
         description: "Industrielle Projektlösung",
         thumbnail: "/images/cover/e210_cover.png",
         images: [
-            "/images/E210/MB13_038_E210_0.png",
-            "/images/E210/MB13_038_E210_1.png",
-            "/images/E210/MB13_038_E210_2.png",
-            "/images/E210/MB13_038_E210_3.png",
-            "/images/E210/MB13_038_E210_4.png",
-            "/images/E210/MB13_038_E210_5.png",
-            "/images/E210/MB13_038_E210_6.png",
-            "/images/E210/MB13_038_E210_7.jpg",
-            "/images/E210/MB13_038_E210_8.jpg",
-            "/images/E210/MB13_038_E210_9.jpg",
-            "/images/E210/MB13_038_E210_10.jpg",
-            "/images/E210/MB13_038_E210_11.jpg"
+            "/images/e210/mb13_038_e210_0.png",
+            "/images/e210/mb13_038_e210_1.png",
+            "/images/e210/mb13_038_e210_2.png",
+            "/images/e210/mb13_038_e210_3.png",
+            "/images/e210/mb13_038_e210_4.png",
+            "/images/e210/mb13_038_e210_5.png",
+            "/images/e210/mb13_038_e210_6.png",
+            "/images/e210/mb13_038_e210_7.jpg",
+            "/images/e210/mb13_038_e210_8.jpg",
+            "/images/e210/mb13_038_e210_9.jpg",
+            "/images/e210/mb13_038_e210_10.jpg",
+            "/images/e210/mb13_038_e210_11.jpg"
         ],
         specs: {
             type: "Steuerungsbau"
@@ -30,17 +30,17 @@ export const projects: Project[] = [
         description: "Energieverteilungssystem",
         thumbnail: "/images/cover/e500_cover.png",
         images: [
-            "/images/E500/e500_0.png",
-            "/images/E500/e500_1.png",
-            "/images/E500/e500_2.png",
-            "/images/E500/E500_3.png",
-            "/images/E500/e500_4.png",
-            "/images/E500/e500_5.png",
-            "/images/E500/e500_6.png",
-            "/images/E500/e500_7.jpg",
-            "/images/E500/e500_8.jpg",
-            "/images/E500/e500_9.jpg",
-            "/images/E500/e500_10.jpg"
+            "/images/e500/e500_0.png",
+            "/images/e500/e500_1.png",
+            "/images/e500/e500_2.png",
+            "/images/e500/e500_3.png",
+            "/images/e500/e500_4.png",
+            "/images/e500/e500_5.png",
+            "/images/e500/e500_6.png",
+            "/images/e500/e500_7.jpg",
+            "/images/e500/e500_8.jpg",
+            "/images/e500/e500_9.jpg",
+            "/images/e500/e500_10.jpg"
         ],
         specs: {
             type: "Energieverteilung"
@@ -52,14 +52,14 @@ export const projects: Project[] = [
         description: "Präzisionsmontage",
         thumbnail: "/images/cover/j1_cover.png",
         images: [
-            "/images/J1/j1_0.png",
-            "/images/J1/j1_1.png",
-            "/images/J1/j1_2.png",
-            "/images/J1/j1_3.png",
-            "/images/J1/j1_4.png",
-            "/images/J1/j1_5.jpg",
-            "/images/J1/j1_6.jpg",
-            "/images/J1/j1_7.jpg"
+            "/images/j1/j1_0.png",
+            "/images/j1/j1_1.png",
+            "/images/j1/j1_2.png",
+            "/images/j1/j1_3.png",
+            "/images/j1/j1_4.png",
+            "/images/j1/j1_5.jpg",
+            "/images/j1/j1_6.jpg",
+            "/images/j1/j1_7.jpg"
         ],
         specs: {
             type: "Anschlusslösung"
@@ -71,12 +71,12 @@ export const projects: Project[] = [
         description: "Zentrale Motorsteuerung",
         thumbnail: "/images/cover/mcc1_cover.png",
         images: [
-            "/images/MCC1/MB17_004_MCC1_0.png",
-            "/images/MCC1/MB17_004_MCC1_1.png",
-            "/images/MCC1/MB17_004_MCC1_2.png",
-            "/images/MCC1/MB17_004_MCC1_3.png",
-            "/images/MCC1/MB17_004_MCC1_4.png",
-            "/images/MCC1/MB17_004_MCC1_5.png"
+            "/images/mcc1/mb17_004_mcc1_0.png",
+            "/images/mcc1/mb17_004_mcc1_1.png",
+            "/images/mcc1/mb17_004_mcc1_2.png",
+            "/images/mcc1/mb17_004_mcc1_3.png",
+            "/images/mcc1/mb17_004_mcc1_4.png",
+            "/images/mcc1/mb17_004_mcc1_5.png"
         ],
         specs: {
             type: "MCC"
